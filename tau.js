abdbf3b257160e2003cac15c438a6d3acc0d13fa
12Thanjo <alfeldman12@gmail.com>
2022-03-01 14:48:44 -0500

    fixed bug with nested for loops

--- tau.js
+++ tau.js
@@ -10,12 +10,12 @@ var dirname = process.argv[2].replaceAll("?", " ");
 var help_color = "yellow";
 var help_log = function(title, description){
 	if(help_color == "yellow"){
-		help_color = "blue"; // actually orange due to my cmd theme
+		help_color = "green"; // actually orange due to my cmd theme
 	}else{
 		help_color = "yellow";
 	}
 
-	cmd.log(title + ": " + cmd.color[help_color] + description, cmd.color.green);
+	cmd.log(title + ": " + cmd.color[help_color] + description);
 }
 
 
@@ -24,13 +24,15 @@ var help = function(){
 	cmd.log('----------------', cmd.color.cyan);
 	help_log("add <plugin> <dependancy>", "add a dependancy to a plugin");
 	help_log('build <plugin> [...args]', "build a tau project");
-	help_log("help", 'shows "help" menu');
+	help_log("help", 'shows "help" menu ( this is the help menu :) )');
 	help_log("init", "initialize a new plugin");
 	help_log("list <plugin>", "list all dependancies of a plugin");
 	help_log("metadata <plugin>", "get all of the metadata of a plugin");
 	help_log('package <plugin> [...args]', "build a tau project from a .js file instead of a .nt file");
 	help_log("remove <plugin> <dependancy>", "remove a dependancy from a plugin");
 	help_log("version <plugin>", "get the version of a plugin");
+
+	console.log();
 
 	process.exit();
 }
